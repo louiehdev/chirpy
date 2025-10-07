aebba7b6d12e51c875bc8610c881f5e5d3887571
chknofthewoods <cotwgames@gmail.com>
2025-10-07 09:47:22 -0600

    Initialize project server code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/louiehdev/chirpy
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Addr: ":8080",
+		Handler: serveMux,
+	}
+
+	server.ListenAndServe()
+}
