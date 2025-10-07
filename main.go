8c0737c5fa716dc18dbd005f37789d27fbd059a7
Louie H <jlouiehagen@gmail.com>
2025-10-07 21:30:00 +0000

    Start on routing and make it method specific

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"sync/atomic"
 )
 
@@ -10,10 +12,36 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	currentHits := strconv.Itoa(int(cfg.fileserverHits.Load()))
+	hitResponse := fmt.Sprintf("Hits: %s", currentHits)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(hitResponse))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(int32(0))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(""))))
-	mux.HandleFunc("/healthz", httpHandler)
+	apiCfg := apiConfig{}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir("")))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /healthz", httpHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
