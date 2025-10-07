865ca0a0b138ccd1e092a868fe94c9aab0f3c3c1
Louie H <jlouiehagen@gmail.com>
2025-10-07 20:29:03 +0000

    Create initial fileserver and http handler code

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(""))))
+	mux.HandleFunc("/healthz", httpHandler)
+
 	server := http.Server{
-		Addr: ":8080",
-		Handler: serveMux,
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
+}
+
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
